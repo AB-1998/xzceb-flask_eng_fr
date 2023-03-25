f898371d337cb16216a1bebd83f460080e6947f4
Your Name <you@example.com>
2023-03-25 09:49:51 -0400

    edeit

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -11,15 +11,28 @@ apikey = os.environ['apikey']
 url = os.environ['url']
 
 
-authenticator = IAMAuthenticator(apikey)
+authenticator = IAMAuthenticator('{apikey}')
 language_translator = LanguageTranslatorV3(
-    version='{version}',
+    version='2018-05-01',
     authenticator=authenticator
 )
 
-language_translator.set_service_url(url)
+language_translator.set_service_url('{url}')
+
 
 def englishToFrench(englishText):
-    #write the code here
+    
+    translation = language_translator.translate(
+    text=englishText,
+    model_id='en-fr').get_result()
+    frenchText=json.dumps(translation, indent=2, ensure_ascii=False)
     return frenchText
-```
+
+def frenchToEnglish(frenchText):
+    
+    translation = language_translator.translate(
+    text=frenchText,
+    model_id='fr-en').get_result()
+    englishText=json.dumps(translation, indent=2, ensure_ascii=False)
+    return englishText
+    ```
