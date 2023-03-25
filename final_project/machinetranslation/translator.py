d8f138ecc96b0a770d68a181619e4d7736e02a03
Your Name <you@example.com>
2023-03-25 06:40:50 -0400

    editing

---
+++ final_project/machinetranslation/translator.py
@@ -0,0 +1,25 @@
+```py
+import json
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+apikey = os.environ['apikey']
+url = os.environ['url']
+
+
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(
+    version='{version}',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url(url)
+
+def englishToFrench(englishText):
+    #write the code here
+    return frenchText
+```
